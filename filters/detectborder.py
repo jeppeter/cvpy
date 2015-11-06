97c5fcee46568ca089496fdd8551c21576be6a61
jeppeter <jeppeter@gmail.com>
2015-11-06 14:21:55 +0800

    to make several type as detect border

--- filters/detectborder.py
+++ filters/detectborder.py
@@ -16,16 +16,21 @@ def ShowBorder(infile):
 		sys.stderr.write('can not open %s as picture\n'%(infile))
 		sys.exit(3)
 	elmsize = 3
+	typearr = [cv2.MORPH_RECT,cv2.MORPH_CROSS,cv2.MORPH_ELLIPSE]
+	typename = ['RECT','CROSS','ELLIPSE']
+	typeint = 0
 	while True:
-		elem = cv2.getStructuringElement(cv2.MORPH_RECT,(elmsize,elmsize))
+		elem = cv2.getStructuringElement(typearr[typeint],(elmsize,elmsize))
 		dilate = cv2.dilate(img,elem)
 		erode = cv2.erode(img,elem)
 
 		result = cv2.absdiff(dilate,erode)
 		retval,result = cv2.threshold(result,40,255,cv2.THRESH_BINARY)
 		result = cv2.bitwise_not(result)
-		cv2.imshow('img',result)
+		imgshow = 'img %s %d'%(typename[typeint],elmsize)
+		cv2.imshow(imgshow,result)
 		k = cv2.waitKey(0)
+		cv2.destroyAllWindows()
 		if k not in [UPKEY,RIGHTKEY,LEFTKEY,DOWNKEY]:
 			break
 		if k == UPKEY :
@@ -34,6 +39,12 @@ def ShowBorder(infile):
 			elmsize -= 1
 			if elmsize < 1 :
 				elmsize = 1
+		elif k == LEFTKEY:
+			typeint -= 1
+			typeint %= len(typearr)
+		elif k == RIGHTKEY:
+			typeint += 1
+			typeint %= len(typearr)
 	return
 
 
