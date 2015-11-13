f614e8508ce14418352a2416163b218ecf4575c7
jeppeter <jeppeter@gmail.com>
2015-11-13 16:24:46 +0800

    to make track bar in canny.py

--- filters/canny.py
+++ filters/canny.py
@@ -9,6 +9,7 @@ LEFTKEY=2424832
 RIGHTKEY=2555904
 AKEY=97
 SKEY=115
+QKEY=113
 
 class CannyArgs:
 	def __init__(self):
@@ -40,6 +41,22 @@ class CannyArgs:
 			self.max = self.min + 1
 		return
 
+	def setmax(self,maxval):
+		self.max = maxval
+		if self.max >= 255:
+			self.max = 255
+		if self.max <= self.min:
+			self.max = self.min + 1
+		return
+
+	def setmin(self,minval):
+		self.min = minval
+		if self.min  <= 0:
+			self.min = 0
+		if self.min >= self.max:
+			self.min = self.max - 1
+		return
+
 def key_usage():
 	sys.stdout.write('UP to increase min\n')
 	sys.stdout.write('DOWN to decrease min\n')
@@ -51,6 +68,10 @@ def key_usage():
 def CannyChange(img,args):
 	return cv2.Canny(img,args.min,args.max)
 
+def nothing(x):
+	sys.stdout.write('set value %s\n'%(x))
+	pass
+
 def CannyShow(infile):
 	try:
 		img = cv2.imread(infile,0)
@@ -61,23 +82,25 @@ def CannyShow(infile):
 	args = CannyArgs()
 	key_usage()
 	simg = img
+
 	while True:
 		dst = CannyChange(simg,args)
 		name = args.name()
+		cv2.namedWindow(name)
+		cv2.createTrackbar('threshlow',name,0,255,nothing)
+		cv2.setTrackbarPos('threshlow',name,args.min)
+		cv2.createTrackbar('threshhigh',name,0,255,nothing)	
+		cv2.setTrackbarPos('threshhigh',name,args.max)
 		cv2.imshow(name,dst)
 		k = cv2.waitKey(0)
+		minval = cv2.getTrackbarPos('threshlow',name)
+		maxval = cv2.getTrackbarPos('threshhigh',name)
+		sys.stdout.write('minval %d maxval %d\n'%(minval,maxval))
 		cv2.destroyAllWindows()
-		if k not in [UPKEY,RIGHTKEY,LEFTKEY,DOWNKEY]:
-			sys.stdout.write('k %d'%(k))
+		args.setmin(minval)
+		args.setmax(maxval)
+		if k == QKEY or k == -1:
 			break
-		if k == UPKEY :
-			args.incmin()
-		elif k == DOWNKEY :
-			args.decmin()
-		elif k == LEFTKEY:
-			args.incmax()
-		elif k == RIGHTKEY:
-			args.decmax()
 	return
 
 def main():
