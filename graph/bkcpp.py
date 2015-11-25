32e0d1d18861c85abb6d62ed23fd2ebfd4bf139d
jeppeter <jeppeter@gmail.com>
2015-11-25 12:45:07 +0800

    to make simple coding

--- graph/bkcpp.py
+++ graph/bkcpp.py
@@ -275,7 +275,7 @@ class BKGraph:
 				s += 'MAXFLOW_ORPHAN'
 				break
 			nodej = self.arcs[aidx].node_head
-			s += '%s'%(GetIdx(nodei))
+			s += '%s'%(GetIdx(aidx))
 			i += 1
 			if nodej == NULL_PTR:
 				break
