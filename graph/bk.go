c8b0df10fb5e1b488bd9cc56a48165f4b9599b00
jeppeter <jeppeter@gmail.com>
2015-11-26 09:10:32 +0800

    coding bk.go for bk init

--- graph/bk.go
+++ graph/bk.go
@@ -5,36 +5,112 @@ import (
 	"log"
 )
 
-type Arc struct {
-	head   *Node
-	next   *Arc
-	sister *Arc
-	r_cap  int
-}
-
 type Node struct {
-	first              *Arc
-	parent             *Arc
-	next               *Node
-	TS                 int
-	DIST               int
-	is_sink            bool
-	is_marked          bool
-	is_in_changed_list bool
-	tr_cap             int
+	name    string /*name of the node ,it is the key to search node*/
+	parent  *Node  /*this is the route recursive pointer ,for example ,if the new route find ,it will recursive find the route by this */
+	next    *Node  /*in the queue */
+	TS      int    /*the handle cycle time */
+	DIST    int    /*if is_sink == False ,it means the distance to source ,if is_sink == True it means the distance to sink*/
+	is_sink bool   /*it means it is in the sink or source map*/
 }
 
-type NodePtr struct {
-	ptr  *Node
-	next *NodePtr
+type NodeMap struct {
+	inner map[string]*Node
 }
 
 type BKGraph struct {
-	nodes     *Node
-	node_last *Node
-	node_max  *Node
-	arcs      *Arc
-	arc_last  *Arc
-	arc_max   *Arc
-	node_num  int
+	nodemap     *NodeMap
+	queue_first *Node
+	queue_last  *Node
+	TIME        int
+	flow        int
+}
+
+func NewNode(name string) *Node {
+	p := &Node{}
+	p.name = name
+	p.parent = NULL
+	p.next = NULL
+	p.TS = 0
+	p.DIST = 0
+	p.is_sink = False
+	return p
+}
+
+func (pnode *Node) GetTS() int {
+	return pnode.TS
+}
+
+func (pnode *Node) SetTS(ts int) {
+	pnode.TS = ts
+}
+
+func (pnode *Node) GetParent() *Node {
+	return pnode.parent
+}
+
+func (pnode *Node) SetParent(parent *Node) {
+	pnode.parent = parent
+	return
+}
+
+func (pnode *Node) GetDist() int {
+	return pnode.DIST
+}
+func (pnode *Node) SetDist(dist int) {
+	pnode.DIST = dist
+	return
+}
+
+func (pnode *Node) GetNext() *Node {
+	return pnode.next
+}
+
+func (pnode *Node) SetNext(next *Node) {
+	pnode.next = next
+	return
+}
+
+func (pnode *Node) GetName() string {
+	return pnode.name
+}
+
+func NewNodeMap() *NodeMap {
+	p := &NodeMap{}
+	p.inner = make(map[string]*Node)
+	return p
+}
+
+func (pmap *NodeMap) GetNode(name string) *Node {
+	pnode, ok := pmap.inner[name]
+	if !ok {
+		return nil
+	}
+	return pnode
+}
+
+func (pmap *NodeMap) AddNode(name string) error {
+	pnode := pmap.GetNode(name)
+	if pnode != nil {
+		return fmt.Errorf("already exists (%s) node", name)
+	}
+
+	pmap.inner[name] = NewNode(name)
+	return nil
+}
+
+func NewBkGraph() *BKGraph {
+	p := &BKGraph{}
+	p.nodemap = NewNodeMap()
+	p.queue_first = nil
+	p.queue_last = nil
+	p.TIME = 0
+	p.flow = 0
+	return p
+}
+
+/**********************************************
+*
+**********************************************/
+func (pbk *BKGraph) InitGraph(caps *StringGraph, neighbour *Neigbour, source string, sink string) error {
 }
