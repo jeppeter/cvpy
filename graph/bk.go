8309c4fad281662d997317ef419efa1190a03567
jeppeter <jeppeter@gmail.com>
2015-11-26 09:40:33 +0800

    to make constant ok

--- graph/bk.go
+++ graph/bk.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 )
 
 type Node struct {
@@ -22,6 +21,7 @@ type BKGraph struct {
 	nodemap     *NodeMap
 	queue_first *Node
 	queue_last  *Node
+	flows       *StringGraph
 	TIME        int
 	flow        int
 }
@@ -71,6 +71,14 @@ func (pnode *Node) SetNext(next *Node) {
 	return
 }
 
+func (pnode *Node)SetSink( val bool) {
+	pnode.is_sink = val
+	return
+}
+func (pnode *Node)GetSink() bool {
+	return pnode.is_sink
+}
+
 func (pnode *Node) GetName() string {
 	return pnode.name
 }
@@ -99,18 +107,52 @@ func (pmap *NodeMap) AddNode(name string) error {
 	return nil
 }
 
+func (pmap *NodeMap)AddNode_NoError(name string) {
+	pnode := pmap.GetNode(name)
+	if pnode == nil{
+		pmap.AddNode(name)
+	}
+	return
+	
+}
+
 func NewBkGraph() *BKGraph {
 	p := &BKGraph{}
 	p.nodemap = NewNodeMap()
 	p.queue_first = nil
 	p.queue_last = nil
+	p.flows = NewStringGraph()
 	p.TIME = 0
 	p.flow = 0
 	return p
 }
 
+
+func (graph *BKGraph)InitSource(pnode *Node) error {
+	pnode.SetSink(false)
+	pnode.SetDist(1)
+	pnode.SetParent(((Node*)))
+	
+}
+
 /**********************************************
-*
+*function :
+*         to init the BKGraph inner structure
+*         by caps and neighbour
 **********************************************/
-func (pbk *BKGraph) InitGraph(caps *StringGraph, neighbour *Neigbour, source string, sink string) error {
+func (graph *BKGraph) InitGraph(caps *StringGraph, neighbour *Neigbour, source string, sink string) error {
+	/*because the two dimensions are not all the same*/
+	for _,k1 := caps.Iter(){
+		graph.nodemap.AddNode_NoError(k1)
+		for _,k2 := caps.IterIdx(k1){
+			graph.nodemap.AddNode_NoError(k2)
+		}
+	}
+
+	/*now search for the source and sink*/
+	pnode := graph.nodemap.GetNode(source)
+	if pnode == nil{
+		return fmt.Errorf("can not find (%s) source in graph",source)
+	}
+
 }
