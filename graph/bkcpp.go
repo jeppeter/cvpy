206260c941d270ed31b570ed42ce0451f409886f
jeppeter <jeppeter@gmail.com>
2015-11-29 13:29:10 +0800

    to add add_tweights in bkcpp.go

--- graph/bkcpp.go
+++ graph/bkcpp.go
@@ -5,6 +5,7 @@ import (
 )
 
 type Arc struct {
+	name   string
 	head   *Node
 	next   *Arc
 	sister *Arc
@@ -13,11 +14,20 @@ type Arc struct {
 
 func NewArc() *Arc {
 	p := &Arc{}
+	p.name = ""
 	p.head = nil
 	p.next = nil
 	p.sister = nil
 	p.r_cap = 0
 	return p
+}
+
+func (parc *Arc) SetName(name string) {
+	parc.name = name
+	return
+}
+func (parc *Arc) GetName() string {
+	return parc.name
 }
 
 func (parc *Arc) SetHead(pnode *Node) {
@@ -57,6 +67,7 @@ func (parc *Arc) SetCap(caps int) {
 }
 
 type Node struct {
+	name    string
 	first   *Arc
 	parent  *Arc
 	next    *Node
@@ -64,6 +75,19 @@ type Node struct {
 	DIST    int
 	is_sink bool
 	tr_cap  int
+}
+
+func NewNode(name string) *Node {
+	p := &Node{}
+	p.name = name
+	p.first = nil
+	p.parent = nil
+	p.next = nil
+	p.TS = 0
+	p.DIST = 0
+	p.is_sink = false
+	p.tr_cap = 0
+	return p
 }
 
 func (pnode *Node) GetFirst() *Arc {
@@ -102,12 +126,60 @@ func (pnode *Node) SetTS(ts int) {
 	return
 }
 
+func (pnode *Node) GetDIST() int {
+	return pnode.DIST
+}
+
+func (pnode *Node) SetDIST(dist int) {
+	pnode.DIST = dist
+	return
+}
+
+func (pnode *Node) IsSink() bool {
+	return pnode.is_sink
+}
+
+func (pnode *Node) SetSink(sink bool) {
+	pnode.is_sink = sink
+	return
+}
+
+func (pnode *Node) GetCap() int {
+	return pnode.tr_cap
+}
+
+func (pnode *Node) SetCap(caps int) {
+	pnode.tr_cap = caps
+	return
+}
+
 type BKGraph struct {
-	nodes             []*Node
-	arcs              []*Arc
-	flow              int
-	maxflow_iteration int
-	orphanlist        []*Node
-	queue_first       *Node
-	queue_last        *Node
+	nodes       map[string]*Node
+	arcs        map[string]*Arc
+	flow        int
+	orphanlist  []*Node
+	queue_first *Node
+	queue_last  *Node
+}
+
+func NewBKGraph() *BKGraph {
+	p := &BKGraph{}
+	p.nodes = make(map[string]*Node)
+	p.arcs = make(map[string]*Arc)
+	p.flow = 0
+	p.orphanlist = []*Node{}
+	p.queue_first = nil
+	p.queue_last = nil
+	return p
+}
+
+func (graph *BKGraph) add_tweights(nodename string, cap_source, cap_sink int) {
+	var pnode *Node
+	var ok bool
+	pnode, ok = graph.nodes[nodename]
+	if !ok {
+		pnode = NewNode(nodename)
+		graph.nodes[nodename] = pnode
+	}
+
 }
