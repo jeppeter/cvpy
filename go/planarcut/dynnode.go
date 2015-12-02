933a7babca81cf13900f8d62b311a70cf1c06eac
jeppeter <jeppeter@gmail.com>
2015-12-02 11:27:45 +0800

    to add dynnode

--- go/planarcut/dynnode.go
+++ go/planarcut/dynnode.go
@@ -1,5 +1,10 @@
 package main
 
+const REV_MASK uint8 = 1
+const TMP_MASK uint8 = 2
+const MAP_MASK uint8 = 4
+const CAP_INF float64 = -1.0
+
 type DynNode struct {
 	parent   *DynNode
 	head     *DynNode
@@ -39,13 +44,114 @@ func (dyn *DynNode) IsLeaf() bool {
 	return false
 }
 
+func (dyn *DynNode) GetReserved() bool {
+	v := dyn.reserved & REV_MASK
+	if v != 0 {
+		return true
+	}
+	return false
+}
+
+func (dyn *DynNode) GetMapping() bool {
+	v := dyn.reserved & MAP_MASK
+	if v != 0 {
+		return true
+	}
+	return false
+}
+
+func (dyn *DynNode) SetMapping(b bool) {
+	if dyn.GetMapping() != b {
+		dyn.reserved ^= MAP_MASK
+	}
+	return
+}
+
+func (dyn *DynNode) SetReserved(b bool) {
+	if dyn.GetReserved() != b {
+		dyn.reserved ^= REV_MASK
+	}
+	return
+
+}
+
+func (dyn *DynNode) NormalizeReserveState() {
+	if !dyn.GetReserved() {
+		return
+	}
+	/*do not normalize for twice*/
+	dyn.SetReserved(false)
+	/*to reverse dyn*/
+	dyn.SetMapping(!dyn.GetMapping())
+	/*swap the left and right*/
+	pn := dyn.left
+	dyn.left = dyn.right
+	dyn.right = pn
+
+	/*swap tail and head*/
+	pn = dyn.head
+	dyn.head = dyn.tail
+	dyn.tail = pn
+
+	/*swap for net min*/
+	c := dyn.netmin
+	dyn.netmin = dyn.netminR
+	dyn.netminR = c
+
+	/*swap for net cost*/
+	c = dyn.netcost
+	dyn.netcost = dyn.netcostR
+	dyn.netcostR = c
+
+	if dyn.right && !dyn.right.IsLeaf() {
+		/*to set the opposite*/
+		dyn.right.SetReserved(!dyn.right.GetReserved())
+
+	}
+
+	if dyn.left && !dyn.left.IsLeaf() {
+		dyn.left.SetReserved(!dyn.left.GetReserved())
+	}
+}
+
+func (dyn *DynNode) GetData() interface{} {
+	return dyn.data
+}
+
 func (dyn *DynNode) RotateRight(gross, grossR float64) {
 	var u, v *DynNode
 	var pnetmin, pnetminR *float64
 	var rstate bool
 
 	if dyn.IsLeaf() {
-		/*it is */
+		/*it is in leaf mode*/
 		return
 	}
+	dyn.NormalizeReserveState()
+	u = dyn
+	v = dyn.left
+
+	if v.IsLeaf() {
+		return
+	}
+
+	v.NormalizeReserveState()
+
+	uold := *dyn
+	vold := *v
+	umapping := uold.GetMapping()
+	vmapping := vold.GetMapping()
+
+	udata := uold.GetData()
+	vdata := vold.GetData()
+	minU := gross
+	minUR := grossR
+	minvold := v.netmin + minU
+	minvoldR := v.netminR + minUR
+
+	costU := u.netcost + minU
+	costUR := u.netcostR + minUR
+	costV := v.netcost + minvold
+	costVR := v.netcostR + minvoldR
+	minvl := 
 }
