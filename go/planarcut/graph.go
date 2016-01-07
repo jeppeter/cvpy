820848e8d1c70aac79b0b3913ab983b97b42d36b
jeppeter <jeppeter@gmail.com>
2016-01-07 11:25:59 +0800

    to make the preflow

--- go/planarcut/graph.go
+++ go/planarcut/graph.go
@@ -535,10 +535,13 @@ func MakePlanarGraph(infile string) (planar *PlanarGraph, err error) {
 }
 
 func (planar *PlanarGraph) preflow() {
+	var infedge *Edge
+	var infvert *Vertice
 	if planar.preflowed > 0 {
 		return
 	}
 
 	/*we now preflowed the */
+	infedge = planar.edges[planar.sinkid]
 
 }
